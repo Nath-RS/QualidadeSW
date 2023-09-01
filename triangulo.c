290e7d5b422175560116ae1728d54dd864be6926
Nath-RS <nathandesouzarodrigues@gmail.com>
2023-08-31 21:04:55 -0300

    saida formatada de acordo ao que foi pedido

--- triangulo.c
+++ triangulo.c
@@ -1,35 +1,43 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-// Fun√ß√£o para verificar se √© poss√≠vel formar um tri√¢ngulo
+// FunÁ„o para verificar se È possÌvel formar um tri‚ngulo
 bool ehTriangulo(double lado1, double lado2, double lado3) {
     if ((lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1)) {
-        return true; // √â poss√≠vel formar um tri√¢ngulo
+        return true; // … possÌvel formar um tri‚ngulo
     } else {
-        return false; // N√£o √© poss√≠vel formar um tri√¢ngulo
+        return false; // N„o È possÌvel formar um tri‚ngulo
     }
 }
 
-// Fun√ß√£o para determinar o tipo de tri√¢ngulo
+// FunÁ„o para determinar o tipo de tri‚ngulo
 const char* tipoTriangulo(double lado1, double lado2, double lado3) {
     if (ehTriangulo(lado1, lado2, lado3)) {
         if (lado1 == lado2 && lado2 == lado3) {
-            return "Equil√°tero";
+            return "O tri‚ngulo È do tipo: Equil·tero";
         } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
-            return "Is√≥sceles";
+            return "O tri‚ngulo È do tipo: IsÛsceles";
         } else {
-            return "Escaleno";
+            return "O tri‚ngulo È do tipo: Escaleno";
         }
     } else {
-        return "Inv√°lido";
+        return "N„o È Tri‚ngulo";
     }
 }
 
 int main() {
     double lado1, lado2, lado3;
 
-    // Solicitar ao usu√°rio os tr√™s lados do tri√¢ngulo
+    // Solicitar ao usu·rio os trÍs lados do tri‚ngulo
     printf("Digite o valor do primeiro lado: ");
     scanf("%lf", &lado1);
 
@@ -39,8 +47,8 @@ int main() {
     printf("Digite o valor do terceiro lado: ");
     scanf("%lf", &lado3);
 
-    // Chamar a fun√ß√£o para determinar o tipo de tri√¢ngulo e imprimir o resultado
-    printf("O tri√¢ngulo √© do tipo: %s\n", tipoTriangulo(lado1, lado2, lado3));
+    // Chamar a funÁ„o para determinar o tipo de tri‚ngulo e imprimir o resultado
+    printf("%s\n", tipoTriangulo(lado1, lado2, lado3));
 
     return 0;
 }
