77e17f0d514d7efb227708dc3791038c30c6337e
Nath-RS <nathandesouzarodrigues@gmail.com>
2023-09-05 13:44:46 -0300

    author=nathan, correcao a teste de cobertura

--- triangulo.testes/trianguloteste.cs
+++ triangulo.testes/trianguloteste.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace triangulo.testes
 {
     public class trianguloteste
@@ -6,26 +8,35 @@ namespace triangulo.testes
         public void ehtriangulo()
         {
             //nao eh trtiangulo
-            Assert.False(Triangulo.ehTriangulo(-3,-3,-3));
-            Assert.False(Triangulo.ehTriangulo(0,0,0));
-            Assert.False(Triangulo.ehTriangulo(50, 0, 0));
-            Assert.False(Triangulo.ehTriangulo(0, 50, 0));
-            Assert.False(Triangulo.ehTriangulo(0, 0, 50));
+            Assert.False(program.ehTriangulo(-3,-3,-3));
+            Assert.False(program.ehTriangulo(0,0,0));
+            Assert.False(program.ehTriangulo(50, 0, 0));
+            Assert.False(program.ehTriangulo(0, 50, 0));
+            Assert.False(program.ehTriangulo(0, 0, 50));
             
             //forma triangulo
-            Assert.True(Triangulo.ehTriangulo(4, 5, 5));
-            Assert.True(Triangulo.ehTriangulo(5, 5, 5));
-            Assert.True(Triangulo.ehTriangulo(7, 5, 6));
+            Assert.True(program.ehTriangulo(4, 5, 5));
+            Assert.True(program.ehTriangulo(5, 5, 5));
+            Assert.True(program.ehTriangulo(7, 5, 6));
 
         }
         [Fact]
         public void tipotriangulo()
         {
-            Assert.Equal("Nao e Triangulo", Triangulo.tipoTriangulo(0, 0, 0));
-            Assert.Equal("O triangulo e do tipo: Equilatero", Triangulo.tipoTriangulo(5, 5, 5));
-            Assert.Equal("O triangulo e do tipo: Isosceles", Triangulo.tipoTriangulo(4, 5, 5));
-            Assert.Equal("O triangulo e do tipo: Escaleno", Triangulo.tipoTriangulo(4, 5, 6));
+            Assert.Equal("Nao e Triangulo", program.tipoTriangulo(0, 0, 0));
+            Assert.Equal("O triangulo e do tipo: Equilatero", program.tipoTriangulo(5, 5, 5));
+            Assert.Equal("O triangulo e do tipo: Isosceles", program.tipoTriangulo(4, 5, 5));
+            Assert.Equal("O triangulo e do tipo: Escaleno", program.tipoTriangulo(4, 5, 6));
         }
 
+        [Fact]
+        public void numValido()
+        {
+            double d = 0;
+            
+            Assert.True(program.numValido("1",d));
+            Assert.False(program.numValido("d", d));
+            Assert.False(program.numValido("", d));
+        }
     }
 }
--- triangulo/Program.cs
+++ triangulo/Program.cs
@@ -6,35 +6,83 @@ using System.Linq;
 
 namespace triangulo
 {
-    internal class program
+    public class program
     {
-       
+
+
+
+        public static bool ehTriangulo(double L1, double L2, double L3)
+        {
+            if ((L1 + L2 > L3) && (L1 + L3 > L2) && (L2 + L3 > L1))
+            {
+                return true; // e possivel formar um triangulo
+            }
+            else
+            {
+                return false; // Nao e possivel formar um triangulo
+            }
+        }
+
+        public static string tipoTriangulo(double lado1, double lado2, double lado3)
+        {
+            if (ehTriangulo(lado1, lado2, lado3))
+            {
+                if (lado1 == lado2 && lado2 == lado3)
+                {
+                    return "O triangulo e do tipo: Equilatero";
+                }
+                else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+                {
+                    return "O triangulo e do tipo: Isosceles";
+                }
+                else
+                {
+                    return "O triangulo e do tipo: Escaleno";
+                }
+            }
+            else
+            {
+                return "Nao e Triangulo";
+            }
+        }
+
+        public static bool numValido(string entrada, double guardar)
+        {
+            if (double.TryParse(entrada, out double d))
+            {
+                guardar = double.Parse(entrada, CultureInfo.InvariantCulture);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void Main(string[] args)
         {
             double[] lado = new double[3];
             string entrada;
             try
             {
-
-                for (int i = 0; i < lado.Length; i++)
+                for (int i = 0; i < 3; i++)
                 {
                     while (true)
                     {
-                        Console.WriteLine("Digite o valor do" + i + "° lado: \n");
+                        Console.WriteLine("Digite o valor do " + (i + 1) + "° lado: \n");
                         entrada = Console.ReadLine();
 
-                        if (double.TryParse(entrada, out double d))
+                        if (!String.IsNullOrEmpty(entrada) && numValido(entrada, lado[i]))
                         {
-                            lado[i] = double.Parse(entrada, CultureInfo.InvariantCulture);
                             break;
                         }
                         else
                         {
-                            Console.WriteLine("Digite um numero válido!");
+                            Console.WriteLine("valor válido!");
                         }
                     }
                 }
-                Console.WriteLine(Triangulo.tipoTriangulo(lado[0], lado[1], lado[2]));
+                Console.WriteLine(tipoTriangulo(lado[0], lado[1], lado[2]));
                 Console.ReadKey();
             }
             catch (Exception ex)
